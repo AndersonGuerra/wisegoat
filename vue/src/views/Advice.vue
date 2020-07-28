97692cdfeef852986df8433e133c9c295f14483a
Anderson Guerra <and.guerra@outlook.com>
2020-07-28 11:20:25 -0300

    Commit de uma versao do wiseGoat feita em vue

---
+++ vue/src/views/Advice.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <div class="subDiv" @click="goHome">
+      <span class="wiseText">Wise</span>
+      <span class="goatText">Goat</span>
+    </div>
+    <div style="display: flex; justify-content: center; margin-top: 44px">
+      <div style="display: flex; width: 800px">
+        <div class="quote">
+          <span class="goatQuote">"{{advice}}"</span>
+          <span class="goatCredit">- A nice bode</span>
+        </div>
+        <div>
+          <img class="img" src="http://placegoat.com/300/300" alt="A nice bode" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import axios from "axios";
+export default {
+  data: () => ({
+    advice: "",
+  }),
+  async created() {
+    let result = await axios.get("https://api.adviceslip.com/advice");
+    this.advice = result.data.slip.advice;
+  },
+  methods: {
+    goHome() {
+      this.$router.push("/");
+    },
+  },
+};
+</script>
+<style>
+.subDiv {
+  cursor: pointer;
+}
+.wiseText {
+  color: #baa193;
+  font-weight: 700;
+  font-size: 24px;
+  padding: 0px;
+}
+.goatText {
+  color: #72005f;
+  font-weight: 700;
+  font-size: 54px;
+  padding: 0px;
+}
+.quote {
+  padding-left: 36px;
+  padding-right: 36px;
+  background: #ece4e1;
+  display: flex;
+  flex-direction: column;
+  height: 300px;
+  justify-content: center;
+  border-bottom-left-radius: 8px;
+  border-top-left-radius: 8px;
+}
+.goatQuote {
+  color: #72005f;
+  font-weight: 700;
+  font-size: 36px;
+}
+.goatCredit {
+  color: #baa193;
+}
+.img {
+  border-bottom-right-radius: 8px;
+  border-top-right-radius: 8px;
+}
+</style>
---
+++ vue/src/views/Home.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="mainDiv">
+    <div class="subDiv">
+      <span class="wiseText">Wise</span>
+      <span class="goatText">Goat</span>
+      <router-link to="/advice" tag="button" class="btn">Pedir conselho</router-link>
+    </div>
+    <img class="wiseGoat" alt="A nice bode" width="30%" src="@/assets/bodezinho.png" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
+
+<style>
+.wiseGoat {
+  animation: floatingGoat 2s infinite;
+  animation-direction: alternate;
+}
+
+@keyframes floatingGoat {
+  100% {
+    transform: translateY(-5%);
+    top: 100%;
+  }
+}
+.mainDiv {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.subDiv {
+  margin-right: 96px;
+  display: flex;
+  flex-direction: column;
+  text-align: start;
+  /* flex-grow: 1 */
+}
+.wiseText {
+  color: #baa193;
+  font-weight: 700;
+  font-size: 48px;
+  padding: 0px;
+}
+.goatText {
+  color: #72005f;
+  font-weight: 700;
+  font-size: 108px;
+  padding: 0px;
+}
+.btn {
+  border-radius: 16px;
+  border: #72005f;
+  background: #72005f;
+  color: white;
+  font-size: 32px;
+  font-weight: 700;
+  padding: 16px 36px 16px 36px;
+  margin-top: 36px;
+}
+.btn:active {
+  border: #4f0042;
+  background: #4f0042;
+}
+</style>
